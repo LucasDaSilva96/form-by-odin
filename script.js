594ff3d146ee74000d3299c9522d3c86a1e4bf4a
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-06-10 18:18:47 +0200

    Create the function that checks if password 1 & 2 is a match, and if that's true - then the user can proceed and create an account.

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ const password_confirm_label = document.querySelector(".confirm_password");
 const create_acc_btn = document.querySelector(".sub-btn");
 
 // Function to check if password 1 === password 2
+let is_a_match = false;
 const check_password_match = function (password1, password2) {
-  let is_a_match = false;
-
   const input_1 = password1.value.split("");
   const input_2 = password2.value.split("");
 
@@ -24,18 +23,18 @@ const check_password_match = function (password1, password2) {
       }
     }
   }
-
   return is_a_match;
 };
 
+// This is for making sure that the user must have password 1 === password 2 before
+// creating an account
 password_confirm_input.addEventListener("input", function () {
+  check_password_match(password_input, password_confirm_input);
   if (check_password_match(password_input, password_confirm_input) === false) {
     password_confirm_label.textContent = "Incorrect match";
+    create_acc_btn.disabled = true;
   } else {
     password_confirm_label.textContent = "Confirm Password";
+    create_acc_btn.disabled = false;
   }
-});
-
-create_acc_btn.addEventListener("click", function (e) {
-  console.log(e);
 });
