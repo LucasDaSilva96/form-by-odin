a595b95d35ac0a73d04e26ab41040b8742f55829
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-06-10 20:02:27 +0200

    Sat up the localStorage for displaying the user's information from the index.html to the confirm.html., created the localStorage.Clear function.

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const password_input = document.getElementById("password");
 const password_confirm_input = document.getElementById("password_confirmation");
 const password_confirm_label = document.querySelector(".confirm_password");
 const create_acc_btn = document.querySelector(".sub-btn");
+const user_first_name = document.getElementById("first_name");
+const user_last_name = document.getElementById("last_name");
+const user_email = document.getElementById("email");
+const user_phone_nr = document.getElementById("phone_number");
+const confirm_h4 = document.querySelector(".confirm-h4");
+const confirm_user_name = document.querySelector(".user-name");
+const confirm_user_email = document.querySelector(".user-email");
+const confirm_user_phone_nr = document.querySelector(".user-phone-nr");
 
 // Function to check if password 1 === password 2
 let is_a_match = false;
@@ -26,15 +34,77 @@ const check_password_match = function (password1, password2) {
   return is_a_match;
 };
 
+// These variables are for storing the user's information
+let f_name;
+let l_name;
+let phone_nr;
+let email;
 // This is for making sure that the user must have password 1 === password 2 before
 // creating an account
-password_confirm_input.addEventListener("input", function () {
-  check_password_match(password_input, password_confirm_input);
-  if (check_password_match(password_input, password_confirm_input) === false) {
-    password_confirm_label.textContent = "Incorrect match";
-    create_acc_btn.disabled = true;
-  } else {
-    password_confirm_label.textContent = "Confirm Password";
-    create_acc_btn.disabled = false;
-  }
-});
+if (window.location.pathname === "/index.html") {
+  password_confirm_input.addEventListener("input", function () {
+    check_password_match(password_input, password_confirm_input);
+    if (
+      check_password_match(password_input, password_confirm_input) === false
+    ) {
+      password_confirm_label.textContent = "Incorrect match";
+      create_acc_btn.disabled = true;
+    } else {
+      password_confirm_label.textContent = "Confirm Password";
+      create_acc_btn.disabled = false;
+    }
+  });
+
+  // *****
+  // This is for saving the user's first name
+  user_first_name.addEventListener("input", function () {
+    f_name = user_first_name.value;
+  });
+
+  // *****
+  // This is for saving the user's last name
+  user_last_name.addEventListener("input", function () {
+    l_name = user_last_name.value;
+  });
+
+  // *****
+  // This is for saving the user's phone nr
+  user_phone_nr.addEventListener("input", function () {
+    phone_nr = user_phone_nr.value;
+  });
+
+  // *****
+  // This is for saving the user's email
+  user_email.addEventListener("input", function () {
+    email = user_email.value;
+  });
+
+  // This is for saving the user's information to the local storage
+  create_acc_btn.addEventListener("click", function () {
+    sessionStorage.setItem("First_name", f_name);
+    sessionStorage.setItem("Last_name", l_name);
+    sessionStorage.setItem("User_phone_nr", phone_nr);
+    sessionStorage.setItem("User_email", email);
+  });
+}
+
+// This function is for changing the welcome-content to match the user's information
+if (window.location.pathname === "/confirm.html") {
+  // This is for making sure that the html is loaded before Document Object Manipulation
+  document.addEventListener("DOMContentLoaded", function () {
+    // This is for retrieving data from the local storage and displaying it on the confirm.html
+    confirm_h4.textContent = `Welcome ${sessionStorage.getItem("First_name")}`;
+    confirm_user_name.textContent = `${sessionStorage.getItem(
+      "First_name"
+    )} ${sessionStorage.getItem("Last_name")}`;
+    confirm_user_email.textContent = `${sessionStorage.getItem("User_email")}`;
+    confirm_user_phone_nr.textContent = `${sessionStorage.getItem(
+      "User_phone_nr"
+    )}`;
+  });
+
+  // This is for clearing the local storage when the user clicks the back button.
+  document.querySelector(".back-btn").addEventListener("click", function () {
+    sessionStorage.clear();
+  });
+}
