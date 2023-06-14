0f85ac07f511b24f4ee04695b39463a3bcabd939
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-06-14 13:26:58 +0200

    Trying to fix the on-load bug. The script is only being execute after the first run.

--- script.js
+++ script.js
@@ -11,35 +11,35 @@ const user_email = document.getElementById("email");
 const user_phone_nr = document.getElementById("phone_number");
 const form = document.getElementById("form");
 
-// Function to check if password 1 === password 2
-let is_a_match = false;
-const check_password_match = function (password1, password2) {
-  const input_1 = password1.value.split("");
-  const input_2 = password2.value.split("");
-
-  if (input_1.length !== 0) {
-    for (let i = 0; i <= input_2.length; i++) {
-      if (input_1.join("") === input_2.join("")) {
-        is_a_match = true;
-        password2.style.backgroundColor = "lightgreen";
-      } else {
-        is_a_match = false;
-        password2.style.backgroundColor = "red";
-      }
-    }
-  }
-  return is_a_match;
-};
-
-// These variables are for storing the user's information
-let f_name;
-let l_name;
-let phone_nr;
-let email;
-
 // This is for triggering the nested eventListener every time that the index.html is loaded
 window.onload = function () {
   if (window.location.pathname.includes("index.html")) {
+    // Function to check if password 1 === password 2
+    let is_a_match = false;
+    const check_password_match = function (password1, password2) {
+      const input_1 = password1.value.split("");
+      const input_2 = password2.value.split("");
+
+      if (input_1.length !== 0) {
+        for (let i = 0; i <= input_2.length; i++) {
+          if (input_1.join("") === input_2.join("")) {
+            is_a_match = true;
+            password2.style.backgroundColor = "lightgreen";
+          } else {
+            is_a_match = false;
+            password2.style.backgroundColor = "red";
+          }
+        }
+      }
+      return is_a_match;
+    };
+
+    // These variables are for storing the user's information
+    let f_name;
+    let l_name;
+    let phone_nr;
+    let email;
+
     // *****
     // This is for saving the user's first name
     user_first_name.addEventListener("input", function () {
